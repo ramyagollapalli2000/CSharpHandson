6fc1f0aadb2f50c4005b18e62b49eaf62d5946de
ramyagollapalli2000 <107127021+ramyagollapalli2000@users.noreply.github.com>
2022-06-13 00:56:54 +0530

    Handson

---
+++ Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForBanking
+{
+    public abstract class Account
+    {
+        public virtual void AccountDetails()
+        {
+            Console.WriteLine("your account details are");
+        }
+        public abstract String WithdrawAmount();
+        public abstract double BalanceEnquiry();
+    }
+
+    interface IDeposit 
+    {
+        int DepositAmount();
+    }
+    interface IRateofInterest
+    {
+        double RoiAmount();
+    }
+}
