b40955eb4601ff6350370adb3f7b6d460863f683
ramyagollapalli2000 <107127021+ramyagollapalli2000@users.noreply.github.com>
2022-06-13 01:15:38 +0530

    handson

---
+++ CurrentAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForBanking
+{
+    public class CurrentAccount:Account,IRateofInterest
+    {
+
+        public  new void AccountDetails()
+        {
+            Console.WriteLine("your current account details are") ;
+        }
+        public override string WithdrawAmount()
+        {
+            return "you can withdraw 10000 per day";
+        }
+        public override double BalanceEnquiry()//abstract method implemented
+        {
+            return 10000;
+        }
+        public double RoiAmount()//interface implemented
+        {
+            return 1.5;
+        }
+    }
+}
