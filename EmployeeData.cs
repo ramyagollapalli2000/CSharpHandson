ddd9a513bab2dc222e9e678c105ca57c7b9c3a2f
ramyagollapalli2000 <107127021+ramyagollapalli2000@users.noreply.github.com>
2022-06-13 01:16:43 +0530

    ado .net handson

---
+++ EmployeeData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CustomerDBconsole
+{
+    public class EmployeeData
+    {
+        public static string connectionstr = "Data Source=DESKTOP-LVVPEMG;Initial Catalog=BankDb;Integrated Security=True";
+        public string InsertEmployee()
+        {
+            Console.WriteLine("enter employee id:");
+            int empid = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter employee name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("enter employee salary:");
+            int salary = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter employee role:");
+            string emprole = Console.ReadLine();
+
+            Console.WriteLine("enter employee Experience:");
+            int empexp = Convert.ToInt32(Console.ReadLine());
+
+            SqlConnection conn = new SqlConnection(connectionstr);
+            string query = "insert into Employee values("+empid+",'"+name+"',"+salary+",'"+emprole+"',"+empexp+")";
+
+            SqlCommand cmd = new SqlCommand(query,conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+
+
+            return "Employee data inserted";
+        }
+        public string UpdateEmployee()
+        {
+            return "";
+        }
+    }
+}
